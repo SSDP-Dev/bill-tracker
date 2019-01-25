a106a4bed2ee9070f894db3b0bd885e29462d2cf
Tyler Williams <tyler@ogdenstudios.xyz>
2019-01-25 10:42:07 -0700

    wipe out pretty much the whole dang thing

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#index"
-  resources :bills
-  post '/follow', to: "bills#bill_follow"
-  get '/following', to: "bills#following"
-  get '/activity', to: "bills#activity"
-end
